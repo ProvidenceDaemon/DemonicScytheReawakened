e20ae863a6a69da9473907d5b4e75ae672c09d06
ProvidenceDaemon <91506764+ProvidenceDaemon@users.noreply.github.com>
2025-07-04 18:22:26 -0500

    bump version number

--- src/main/java/com/omicron/demonic_scythe/DemonicScythe.java
+++ src/main/java/com/omicron/demonic_scythe/DemonicScythe.java
@@ -14,7 +14,7 @@ import java.io.File;
 public class DemonicScythe
 {
     public static final String MODID = "demonic_scythe";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.4";
     
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
